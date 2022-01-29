74371ff3ff09d8469be71276085c2356b4805dc6
Karan <brarkaran938@gmail.com>
2022-01-28 21:47:31 -0600

    Added a template for the index view in poll app

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,19 @@
+import imp
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Question
+from django.template import loader
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, World. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
-def detail(rqeuest, question_id):
+def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
